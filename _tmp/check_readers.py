0fac10a19fad6a940c1691c810d91c24d51c4c2d
QingGo <zyqingjohn@qq.com>
2026-07-17 22:25:49 +0800

    chore: cleanup temp files

--- _tmp/check_readers.py
+++
@@ -1,6 +0,0 @@
-import json
-d = json.load(open('C:/Users/minam/code/stardew-bilin/_tmp/font-zh/SpriteFont1.zh-CN.json', encoding='utf-8'))
-for i, r in enumerate(d['readers']):
-    t = r['type']
-    v = r['version']
-    print(f'  {i}: {t} v{v}')
